ae9b356250531c003bf34416d050932095be510a
Yuliia Kovalchuk <iuliia1702@gmail.com>
2022-06-22 15:55:19 +0300

    add-libs

---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const axios = require('axios');
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
